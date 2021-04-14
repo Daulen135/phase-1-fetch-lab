7f80c57c4e7cf72450161d0120bd1f538eea81a7
daulen <daulenz@gmail.com>
2021-04-13 19:16:46 -0500

    commit tree 64ad7318f57c65e42d76b88afe0cff56e77ac69f

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let books = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(renderBooks);
+  return books
 }
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
